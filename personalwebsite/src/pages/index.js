0479a601ad838aaa235a72fb0cc3251e8024ec38
Shane Schmaltz <stschmaltz@gmail.com>
2019-01-31 19:15:46 -0700

    base set up for deploying and developing

--- personalwebsite/src/pages/index.js
+++ personalwebsite/src/pages/index.js
@@ -7,13 +7,25 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO
+      title="Home"
+      keywords={[
+        `gatsby`,
+        `application`,
+        `react`,
+        `Shane Schmaltz`,
+        `Shane`,
+        `Schmaltz`,
+        `Calgary`,
+        `Software Developer`,
+        `Software`,
+        `Developer`,
+      ]}
+    />
+    <h1>Shane Schmaltz</h1>
+    <p>Software Developer</p>
+    <p>Creating good first impressions on the web</p>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
