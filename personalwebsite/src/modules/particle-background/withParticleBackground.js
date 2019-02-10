c5609887ce6966740af38b04e32e19c77dde4174
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-10 12:46:43 -0700

    make the particle background kinda cool, css tweaks on landing

--- personalwebsite/src/modules/particle-background/withParticleBackground.js
+++ personalwebsite/src/modules/particle-background/withParticleBackground.js
@@ -3,9 +3,57 @@ import Particles from 'react-particles-js'
 import particleParams from './particle-params.json'
 
 export const withParticleBackground = WrappedComponent => props => {
+  const params = {
+    ...particleParams,
+    particles: { ...particleParams.particles, color: { value: '#0D5A78' } },
+  }
+  console.log(params)
   return (
     <div>
-      <Particles params={particleParams} style={{ position: 'absolute' }} />
+      <Particles
+        params={params}
+        style={{ pointerEvents: 'none', position: 'absolute' }}
+      />
+      <Particles
+        params={{
+          ...particleParams,
+          particles: {
+            ...particleParams.particles,
+            color: { value: '#8B0915' },
+          },
+        }}
+        style={{ pointerEvents: 'none', position: 'absolute' }}
+      />
+      <Particles
+        params={{
+          ...particleParams,
+          particles: {
+            ...particleParams.particles,
+            color: { value: '#BD1659' },
+          },
+        }}
+        style={{ pointerEvents: 'none', position: 'absolute' }}
+      />
+      <Particles
+        params={{
+          ...particleParams,
+          particles: {
+            ...particleParams.particles,
+            color: { value: '#620A4D' },
+          },
+        }}
+        style={{ pointerEvents: 'none', position: 'absolute' }}
+      />
+      <Particles
+        params={{
+          ...particleParams,
+          particles: {
+            ...particleParams.particles,
+            color: { value: '#4E7010' },
+          },
+        }}
+        style={{ pointerEvents: 'none', position: 'absolute' }}
+      />
       <WrappedComponent {...props} />
     </div>
   )
