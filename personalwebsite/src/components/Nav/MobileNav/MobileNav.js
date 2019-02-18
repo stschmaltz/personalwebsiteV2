09635f2e5cb83b8eed71d0261b0dd8f7d5e6d31d
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-18 13:04:44 -0700

    add icons to the bottom nav

--- personalwebsite/src/components/Nav/MobileNav/MobileNav.js
+++ personalwebsite/src/components/Nav/MobileNav/MobileNav.js
@@ -3,15 +3,22 @@ import PropTypes from 'prop-types'
 import './MobileNav.css'
 import { Link } from 'gatsby'
 import MobileNavItem from './MobileNavItem'
+import {
+  FaQuestion,
+  FaHome,
+  FaFileAlt,
+  FaThLarge,
+  FaMailBulk,
+} from 'react-icons/fa'
 
 const MobileNav = props => {
   return (
     <div className="mobile-nav-nar__main-container">
-      <MobileNavItem title="About" path="/about/" />
-      <MobileNavItem title="Resume" path="/resume/" />
-      <MobileNavItem title="Home" path="/" />
-      <MobileNavItem title="Portfolio" path="/portfolio/" />
-      <MobileNavItem title="Contact" path="/contact/" />
+      <MobileNavItem title="Home" path="/" Icon={FaHome} />
+      <MobileNavItem title="About" path="/about/" Icon={FaQuestion} />
+      <MobileNavItem title="Resume" path="/resume/" Icon={FaFileAlt} />
+      <MobileNavItem title="Portfolio" path="/portfolio/" Icon={FaThLarge} />
+      <MobileNavItem title="Contact" path="/contact/" Icon={FaMailBulk} />
     </div>
   )
 }
--- personalwebsite/src/components/Nav/MobileNav/MobileNavItem.js
+++ personalwebsite/src/components/Nav/MobileNav/MobileNavItem.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import './MobileNav.css'
 
-const MobileNavItem = ({ title, path }) => {
+const MobileNavItem = ({ title, path, Icon }) => {
   return (
     <div className="mobile-nav-bar__nav-item-outer-container">
       <Link className="mobile-nav-bar__nav-item-link" to={path}>
         <div className="mobile-nav-bar__nav-item-inner-container">
-          <div className="mobile-nav-bar__nav-item-icon" />
+          <Icon className="mobile-nav-bar__nav-item-icon" />
           <span className="mobile-nav-bar__nav-item-text"> {title} </span>
         </div>
       </Link>
