571659ef25f990bce69c243ea622e39e732a1a28
Shane Schmaltz <stschmaltz@gmail.com>
2019-05-12 01:20:51 -0600

    upload resume pdf (lacks styling and download still) pdf seems find. MD would be better though

--- personalwebsite/src/modules/resume-page/ResumePage.js
+++ personalwebsite/src/modules/resume-page/ResumePage.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Document } from 'react-pdf'
+import React, { Component } from 'react'
+import { Document, Page } from 'react-pdf'
+import resumeFile from '../../images/Shane-Schmaltz-Resume.pdf'
 
-const ResumePage = props => {
-  return (
-    <div>
-      <Document file="" />
-    </div>
-  )
+class ResumePage extends Component {
+  state = {
+    file: resumeFile,
+    numPages: null,
+  }
+
+  onFileChange = event => {
+    this.setState({
+      file: event.target.files[0],
+    })
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages })
+  }
+
+  render() {
+    const { file, numPages } = this.state
+
+    return (
+      <div className="Example">
+        <div className="Example__container">
+          <div className="Example__container__document">
+            <Document file={file} onLoadSuccess={this.onDocumentLoadSuccess}>
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              ))}
+            </Document>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 ResumePage.propTypes = {}
--- personalwebsite/src/pages/resume.js
+++ personalwebsite/src/pages/resume.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
 import withParticleBackground from '../modules/particle-background/withParticleBackground'
+import ResumePageContent from '../modules/resume-page/ResumePage'
 
 const ResumePage = () => {
-  return <Layout>Resume</Layout>
+  return (
+    <Layout>
+      <ResumePageContent />
+    </Layout>
+  )
 }
 
 export default withParticleBackground(ResumePage)
