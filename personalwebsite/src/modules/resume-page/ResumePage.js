99726b750e2af3563b4f677184d7c99efc65f8a4
Shane Schmaltz <stschmaltz@gmail.com>
2019-05-12 00:50:20 -0600

    skeleton resume page

---
+++ personalwebsite/src/modules/resume-page/ResumePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Document } from 'react-pdf'
+
+const ResumePage = props => {
+  return (
+    <div>
+      <Document file="" />
+    </div>
+  )
+}
+
+ResumePage.propTypes = {}
+
+export default ResumePage
