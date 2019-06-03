143aeafe9ab87821e99f803665c5ef88f1c64ea2
Shane Schmaltz <stschmaltz@gmail.com>
2019-06-03 02:34:25 -0600

    start to make some progress on about page

--- personalwebsite/src/modules/about-page/components/AboutPageMainContent.js
+++ personalwebsite/src/modules/about-page/components/AboutPageMainContent.js
@@ -16,9 +16,35 @@ const AboutPageMainContent = props => {
         </Link>
       </p>
       <div className="about-page-main_timeline-container">
-        <span className="about-page-main_timeline-title">Timeline</span>
         <div className="about-page-main_timeline-inner-container">
+          <div className="about-page-main_timeline-year-container">
+            <div className="about-page-main_timeline-year-item-container">
+              <div className="divider" />
+              <span>2015</span>
+            </div>
+            <div className="about-page-main_timeline-year-item-container">
+              <div className="divider" />
+              <span>2016</span>
+            </div>
+            <div className="about-page-main_timeline-year-item-container">
+              <div className="divider" />
+              <span>2017</span>
+            </div>
+            <div className="about-page-main_timeline-year-item-container">
+              <div className="divider" />
+              <span>2018</span>
+            </div>
+            <div className="about-page-main_timeline-year-item-container">
+              <div className="divider" />
+              <span>2019</span>
+            </div>
+            <div className="about-page-main_timeline-year-item-container" />
+          </div>
           <div className="about-page-main_timeline-bar">
+            <span>Graduated University</span>
+            <span>Started at Replicon as Test Automation Engineer</span>
+            <span>Became a developer at Replicon</span>
+            <span>Became a </span>
             <div className="about-page-main_timeline-item about-page-main_timeline-university" />
             <div className="about-page-main_timeline-item about-page-main_timeline-replicon-qa" />
             <div className="about-page-main_timeline-item about-page-main_timeline-replicon-dev" />
