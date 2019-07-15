0b87471a183fc56837b39422a89638f22f4d652e
Shane Schmaltz <stschmaltz@gmail.com>
2019-07-14 21:27:44 -0600

    rebase errors

---
+++ personalwebsite/src/modules/about-page/components/TimelineData/timeline.js
@@ -0,0 +1,38 @@
+export const timelineData = [
+  {
+    secondaryText: 'April 2015',
+    titleText: 'Graduated University',
+    eventName: 'university',
+    location: 'University of Calgary',
+    dateRange: 'Apr 2010 - Sep 2019',
+    description:
+      'I graduated from the University of Calgary with a Bachelors of Science in Computer Science.',
+  },
+  {
+    secondaryText: 'Replicon | May 2015',
+    titleText: 'Test Automation Engineer',
+    eventName: 'replicon-qa',
+    location: 'Replicon Inc.',
+    dateRange: 'May 2010 - May 2011',
+    description:
+      'Replicon was my first introduction to programming in the real world and grew to truly love it. I started in QA writing acceptance tests in selenium and I learned the process being creating enterprise software using agile methodologies.',
+  },
+  {
+    secondaryText: 'Replicon | May 2016',
+    titleText: 'Full-Stack Developer',
+    eventName: 'replicon-dev',
+    location: 'Replicon Inc.',
+    dateRange: 'May 2011 - Mar 2019',
+    description:
+      'After a year of QA, I was eager for a new  challenge... Development! My first year as a dev was  spent mostly working on a data streaming project involving Kafka and Kotlin. After this I started learning ReactJS and dove deep into the front-end world. I remained fullstack and working with many AWS resources but this became my passion and area of obsession.',
+  },
+  {
+    secondaryText: 'Osprey | March 2019',
+    titleText: 'Front-End Developer',
+    eventName: 'osprey',
+    location: 'Osprey informatics',
+    dateRange: 'Mar 2010 - Present',
+    description:
+      "As I neared four years at Replicon, I felt that I was ready for another challenge and switched companies to Osprey Informatics. Replicon was a midsize company with 500 employee and Osprey was about 20 when I joined. It's been incredibly different and also a period of rapid growth for me. I do EmberJS and am the primary front-end developer, effectively taking ownership of much of the front-end",
+  },
+]
